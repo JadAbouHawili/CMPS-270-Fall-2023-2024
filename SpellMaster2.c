5ae56bcdd5825f8468c081c80452e179d25c332f
WalidTabbara <143382686+WalidTabbara@users.noreply.github.com>
2023-11-12 00:48:08 +0200

    Update SpellMaster2.c

--- SpellMaster2.c
+++ SpellMaster2.c
@@ -3,9 +3,8 @@
 #include <stdbool.h>
 
 char** ArrayOfSpells;
-int numberOfWords;
-int const sizeOfSpells = 150;
 int numberOfSpells;
+int const sizeOfSpells = 150;
 
 struct Word{
     char * word;
@@ -41,23 +40,35 @@ struct Word* createWord(char word[]){
 
     return newWord;
 }
+
 void readSpells(){
     int i;
+
+
     FILE * fpointer;
+
     fpointer = fopen("spells.txt", "r");
-    fscanf(fpointer , "%d", &numberOfSpells);
+
+
     if(fpointer == NULL){
         printf("Could Not Open File! \n");
         numberOfSpells = 0;
         return;
     }
+
+
+
+
     if(fscanf(fpointer, "%d", &numberOfSpells) != 1){
         printf("Please make sure the file starts with an integer to denote the number of Spells.\n");
         numberOfSpells = 0;
         fclose(fpointer);
         return;
     }
+
+
     ArrayOfSpells = (char **)malloc(numberOfSpells * sizeof(char *));
+
     for(i=0; i<numberOfSpells; ++i){
         ArrayOfSpells[i] = (char* )malloc((sizeOfSpells+1) * sizeof(char));
         fscanf(fpointer, "%s", ArrayOfSpells[i]);
@@ -65,6 +76,7 @@ void readSpells(){
 
     fclose(fpointer);
 }
+
 struct Graph * createGraph(int numWords){
     struct Graph * Graph = (struct Graph* ) malloc(sizeof(struct Graph));
 
@@ -130,57 +142,34 @@ void addEdge(struct Graph * Graph, char source[], char destination[]){
         }
     }
 }
+
 struct Graph* buildGraph(){
     struct Graph* Graph = createGraph(numberOfSpells);
-    
-    char currentWord[150];
-    char otherWord[150];
+    char currentWord[sizeOfSpells];
 
     int i,j,k;
     for(i=0; i<numberOfSpells; ++i){
-        k = 0;
-        while(ArraysOfSpells[i][k] != '\0'){
-            currentWord[k] = ArraysOfSpells[i][k];
-            ++k;
-        }
-        Graph->adjList[i] = createWord(currentWord);
+        Graph->adjList[i] = createWord(ArrayOfSpells[i]);
     }
+
     for(i=0; i<numberOfSpells; ++i){
-        k = 0;
-        while(ArrayOfSpells[i][k] != '\0'){
-            currentWord[k] = ArrayOfSpells[i][k];
-            ++k;
-        }
-        currentWord[k] = '\0';
         for(j=0; j<numberOfSpells; ++j){
             if(i != j){
-                k = 0;
-                while(ArrayOfSpells[i][k] != '\0'){
-                    otherWord[k] = ArrayOfSpells[i][k];
-                    ++k;
-                }
-                otherWord[k] = '\0';
-                if(condition(currentWord, otherWord)){
-                    addEdge(Graph, currentWord, otherWord);
+                if(condition(ArrayOfSpells[i], ArrayOfSpells[j])){
+                    addEdge(Graph, ArrayOfSpells[i], ArrayOfSpells[j]);
                 }
             }
         }
-        free(currentWord);
-        free(otherWord);
     }
     return Graph;
 }
+
 void printGraph(struct Graph* Graph){
     int i;
     for(i=0; i<Graph->numWords; ++i){
         struct Word* current= Graph->adjList[i];
         while(current != NULL){
-            if(current->used){
-                printf("[%s] (used) ->", current->word);
-            }
-            else {
-                printf("[%s] (unused) ->", current->word);
-            }
+            printf("[%s]-> ", current->word);
             current = current->nextWord;
         }
         printf("\n");
@@ -242,7 +231,6 @@ void printArray(int A[], int size){
 // Easy Mode
 int Kazdoora(struct Graph* Graph, bool myTurn, int lastCharCount[], int firstCharCount[]){
     char oppChoice[150];
-    // from the perspective of the bot
     char myChoice[150];
     int i;
     if(!myTurn){
@@ -300,7 +288,6 @@ int Kazdoora(struct Graph* Graph, bool myTurn, int lastCharCount[], int firstCha
         }
         updateGraph(Graph, myChoice);
         updateGraph(Graph, oppChoice);
-        printGraph(Graph);
     }
     else{
         i = 0;
@@ -311,7 +298,6 @@ int Kazdoora(struct Graph* Graph, bool myTurn, int lastCharCount[], int firstCha
         myChoice[i] = '\0';
         printf("My Choice: %s", myChoice);
         updateGraph(Graph, myChoice);
-        printGraph(Graph);
 
         if(lastCharCount[lastCharOf(oppChoice) - 'a'] > 0) lastCharCount[lastCharOf(oppChoice) - 'a']--;
         if(firstCharCount[oppChoice[0] - 'a'] >0 ) firstCharCount[oppChoice[0] - 'a']--;
@@ -377,66 +363,11 @@ int Kazdoora(struct Graph* Graph, bool myTurn, int lastCharCount[], int firstCha
             }
             updateGraph(Graph, myChoice);
             updateGraph(Graph, oppChoice);
-            printGraph(Graph);
         }   
 }
 int main(){
-    struct Graph* Graph = createGraph(9);
-
-    char words[20][20] = {"einstein", "newton", "number", "nice", "reimann", "shorter", "yes", "energy", "xiolio"};
-
-    int lastCharCount[26];
-    int firstCharCount[26];
-    int i;
-    int j;
-    for(i=0; i<26; ++i){
-        lastCharCount[i] = 0;
-    }
-    for(i=0; i<26; ++i){
-        firstCharCount[i] = 0;
-    }
-
-    for(i=0; i<9; ++i){
-        j = 0;
-        while(words[i][j] != '\0'){
-            ++j;
-        }
-        lastCharCount[words[i][j-1] - 'a']++;
-        firstCharCount[words[i][0] - 'a']++;
-    }
-    for(i=0; i<26; ++i){
-        printf("%d ", lastCharCount[i]);
-    }
-    printf("\n");
-    for(i=0; i<26; ++i){
-        printf("%d ", firstCharCount[i]);
-    }
-    printf("\n");
-    Graph->adjList[0] = createWord("einstein");
-    Graph->adjList[1] = createWord("newton");
-    Graph->adjList[2] = createWord("number");
-    Graph->adjList[3] = createWord("nice");
-    Graph->adjList[4] = createWord("reimann");
-    Graph->adjList[5] = createWord("shorter");
-    Graph->adjList[6] = createWord("yes");
-    Graph->adjList[7] = createWord("energy");
-    Graph->adjList[8] = createWord("xiolio");
-
-    addEdge(Graph, "einstein", "newton");
-    addEdge(Graph, "einstein", "number");
-    addEdge(Graph, "einstein", "nice");
-    addEdge(Graph, "newton", "number");
-    addEdge(Graph, "newton", "nice");
-    addEdge(Graph, "number", "reimann");
-    addEdge(Graph, "nice", "einstein");
-    addEdge(Graph, "nice", "energy");
-    addEdge(Graph, "reimann", "newton");
-    addEdge(Graph, "reimann", "number");
-    addEdge(Graph, "reimann", "nice");
-    addEdge(Graph, "shorter", "reimann");
-    addEdge(Graph, "yes", "shorter");
-    addEdge(Graph, "energy", "yes");
+    readSpells();
+    struct Graph* Graph = buildGraph();
     printGraph(Graph);
-    int x = Kazdoora(Graph, false, lastCharCount, firstCharCount);
     freeGraph(Graph);
 }
